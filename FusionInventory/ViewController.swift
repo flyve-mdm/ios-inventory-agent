208a97343c7bc5d138d33739ef96256f39aebb5d
Hector Rondon <hrondon@teclib.com>
2017-06-22 12:04:57 -0400

    init project

---
+++ FusionInventory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FusionInventory
+//
+//  Created by Hector Rondon on 22/06/17.
+//  Copyright © 2017 Teclib. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
