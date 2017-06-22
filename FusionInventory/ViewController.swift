c93a58e1d68ecc3441cad7477551ecbe7ba32451
Hector Rondon <hrondon@teclib.com>
2017-06-22 15:02:27 -0400

    add header license

--- FusionInventory/ViewController.swift
+++ FusionInventory/ViewController.swift
@@ -1,10 +1,29 @@
-//
-//  ViewController.swift
-//  FusionInventory
-//
-//  Created by Hector Rondon on 22/06/17.
-//  Copyright © 2017 Teclib. All rights reserved.
-//
+/*
+ *   Copyright © 2017 Teclib. All rights reserved.
+ *
+ * ViewController.swift is part of FusionInventory
+ *
+ * FusionInventory is a subproject of Flyve MDM. Flyve MDM is a mobile
+ * device management software.
+ *
+ * FusionInventory is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Example is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * ------------------------------------------------------------------------------
+ * @author    Hector Rondon
+ * @date      22/06/17
+ * @copyright Copyright © 2017 Teclib. All rights reserved.
+ * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @link      https://github.com/flyve-mdm/fusioninventory-ios
+ * @link      https://flyve-mdm.com
+ * ------------------------------------------------------------------------------
+ */
 
 import UIKit
 
