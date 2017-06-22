94b5603e4cea6a92ffc22d97c1e6cd2210a80da3
Hector Rondon <hrondon@teclib.com>
2017-06-22 14:04:48 -0400

    add view inventory post content

--- FusionInventory/ViewController.swift
+++ FusionInventory/ViewController.swift
@@ -9,17 +9,95 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    let logoImageView: UIImageView = {
+        
+        let imageView = UIImageView(image: UIImage(named: "logo"))
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        
+        return imageView
+    }()
+    
+    let messageLabel: UILabel = {
+        
+        let label = UILabel()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = ""
+        label.sizeToFit()
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.backgroundColor = .clear
+        label.textColor = .gray
+        
+        return label
+    }()
+    
+    let loadingIndicatorView: UIActivityIndicatorView = {
+        
+        let loading = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+        
+        loading.translatesAutoresizingMaskIntoConstraints = false
+        loading.hidesWhenStopped = true
+        
+        return loading
+    }()
+    
+    lazy var postButton: UIButton = {
+        
+        let button = UIButton(type: .custom)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.backgroundColor = UIColor.init(red: 6.0/255.0, green: 135.0/255.0, blue: 133.0/255.0, alpha: 1.0).cgColor
+        button.setTitle("Post XML Inventory", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.addTarget(self, action: #selector(self.sendXmlInventory), for: .touchUpInside)
+        return button
+    }()
+    
+    override func loadView() {
+        super.loadView()
+        
+        setupViews()
+        addConstraints()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setupViews() {
+        
+        view.backgroundColor = .white
+        
+        view.addSubview(messageLabel)
+        view.addSubview(logoImageView)
+        view.addSubview(loadingIndicatorView)
+        view.addSubview(postButton)
     }
-
-
+    
+    func addConstraints() {
+        
+        
+        logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        logoImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        messageLabel.bottomAnchor.constraint(equalTo: logoImageView.topAnchor, constant: -16).isActive = true
+        messageLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24).isActive = true
+        messageLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24).isActive = true
+        
+        loadingIndicatorView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        loadingIndicatorView.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 16).isActive = true
+        
+        postButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -24).isActive = true
+        postButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24).isActive = true
+        postButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24).isActive = true
+        
+        postButton.heightAnchor.constraint(equalToConstant: 50)
+        
+    }
+    
+    func sendXmlInventory() {
+    
+    }
 }
 
