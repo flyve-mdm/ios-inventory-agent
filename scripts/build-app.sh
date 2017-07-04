f5a05478eaf096b4d4c1b44c787cd4fc9ce6a9b3
Hector Rondon <hrondon@teclib.com>
2017-07-04 12:46:21 -0400

    fix build app script

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -4,4 +4,4 @@
 xcodebuild -workspace FlyveMDMInventoryAgent.xcworkspace -scheme FlyveMDMInventoryAgent -destination generic/platform=iOS clean
 
 # Create Archive
-xcodebuild -workspace FlyveMDMInventoryAgent.xcworkspace -scheme FlyveMDMInventoryAgent -sdk iphoneos -configuration Release archive -archivePath $PWD/build/FlyveMDMInventoryAgent.xcarchive OTHER_CODE_SIGN_FLAGS="--keychain ~/Library/Keychains/ios-build.keychain" CODE_SIGN_IDENTITY="iPhone Distribution: s42, C.A. (G3UM9C856M)"
+#xcodebuild -workspace FlyveMDMInventoryAgent.xcworkspace -scheme FlyveMDMInventoryAgent -sdk iphoneos -configuration Release archive -archivePath $PWD/build/FlyveMDMInventoryAgent.xcarchive OTHER_CODE_SIGN_FLAGS="--keychain ~/Library/Keychains/ios-build.keychain" CODE_SIGN_IDENTITY="iPhone Distribution: s42, C.A. (G3UM9C856M)"
