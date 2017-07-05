92ff922a9b3337f4698d4e3c437f92bb1faa11af
Hector Rondon <hrondon@teclib.com>
2017-07-05 14:38:02 -0400

    create unit test testLocalize

--- FlyveMDMInventoryAgentTests/FlyveMDMInventoryAgentTests.swift
+++ FlyveMDMInventoryAgentTests/FlyveMDMInventoryAgentTests.swift
@@ -40,16 +40,10 @@ class FlyveMDMInventoryAgentTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testLocalize() {
+        
+        XCTAssertNotEqual("app_name".localized, "app_name", "Localizable file valid")
+
     }
     
 }
