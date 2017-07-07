c39c0ec5ee6e6099a4bd6fb967c9198c4fa56dee
Hector Rondon <hrondon@teclib.com>
2017-07-07 19:59:07 -0400

    chore(TestFlight): distribute the app for testing (#31)
    * add environment variables to .gitignore
    * add deploy option to .travis.yml
    * remove scripts build-app.sh
    * remove scripts create-ipa.sh
    * update build project
    * add fastlane/log/ to .gitignore
    * fix script add-key.sh
    * fix script remove-key.sh
    * create script for upload to TestFlight
    * fix validate .travis.yml file
    
    Close: https://github.com/flyve-mdm/flyve-mdm-ios-inventory-agent/pull/24

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -6,26 +6,26 @@ openssl aes-256-cbc -k "$KEYCHAIN_PASSWORD" -in $CERTIFICATES_PATH/dist.cer.enc 
 openssl aes-256-cbc -k "$KEYCHAIN_PASSWORD" -in $CERTIFICATES_PATH/dist.p12.enc -d -a -out $CERTIFICATES_PATH/dist.p12
 
 # Create the keychain with a password
-security create-keychain -p $KEYCHAIN_PASSWORD ios-build.keychain
-security add-certificates -k ios-build.keychain $CERTIFICATES_PATH/apple.cer $CERTIFICATES_PATH/dist.cer
+security create-keychain -p $KEYCHAIN_PASSWORD $KEYCHAIN_NAME
+security add-certificates -k $KEYCHAIN_NAME $CERTIFICATES_PATH/apple.cer $CERTIFICATES_PATH/dist.cer
 
 # Make the custom keychain default, so xcodebuild will use it for signing
-# security list-keychains -d user -s ios-build.keychain
-security default-keychain -s ios-build.keychain
+# security list-keychains -d user -s $KEYCHAIN_NAME
+security default-keychain -s $KEYCHAIN_NAME
 
 # Unlock the keychain
-security unlock-keychain -p $KEYCHAIN_PASSWORD ios-build.keychain
+security unlock-keychain -p $KEYCHAIN_PASSWORD $KEYCHAIN_NAME
 
 # Set keychain timeout to 1 hour for long builds
 # see http://www.egeek.me/2013/02/23/jenkins-and-xcode-user-interaction-is-not-allowed/
-security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
+security set-keychain-settings -t 3600 -l ~/Library/Keychains/$KEYCHAIN_NAME
 
 # Add certificates to keychain and allow codesign to access them
-security import $CERTIFICATES_PATH/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import $CERTIFICATES_PATH/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import $CERTIFICATES_PATH/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P "$KEYCHAIN_PASSWORD" -T /usr/bin/codesign
+security import $CERTIFICATES_PATH/apple.cer -k ~/Library/Keychains/$KEYCHAIN_NAME -T /usr/bin/codesign
+security import $CERTIFICATES_PATH/dist.cer -k ~/Library/Keychains/$KEYCHAIN_NAME -T /usr/bin/codesign
+security import $CERTIFICATES_PATH/dist.p12 -k ~/Library/Keychains/$KEYCHAIN_NAME -P "$KEYCHAIN_PASSWORD" -T /usr/bin/codesign
 
-security set-key-partition-list -S apple-tool:,apple: -s -k $KEYCHAIN_PASSWORD ios-build.keychain
+security set-key-partition-list -S apple-tool:,apple: -s -k $KEYCHAIN_PASSWORD $KEYCHAIN_NAME
 
 # Put the provisioning profile in place
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
--- scripts/remove-key.sh
+++ scripts/remove-key.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # Delete custom keychain
-security delete-keychain ios-build.keychain
+security delete-keychain $KEYCHAIN_NAME
 rm -f "~/Library/MobileDevice/Provisioning\ Profiles/$PROFILE_UUID.mobileprovision"
