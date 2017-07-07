64fd7fb0922f9369190bb3f0104d947826257835
Hector Rondon <hrondon@teclib.com>
2017-07-07 19:33:00 -0400

    remove scripts create-ipa.sh

--- scripts/create-ipa.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Create IPA file
-xcodebuild -exportArchive -archivePath ./build/FlyveMDMInventoryAgent.xcarchive -exportPath ./build -exportOptionsPlist $SCRIPT_PATH/exportPlist.plist
