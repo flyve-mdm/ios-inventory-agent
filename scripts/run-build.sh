d2ff73c2c75125aef6bb6e238190642f805ae49b
Hector Rondon <hrondon@teclib.com>
2017-07-10 14:58:19 -0400

    chore(TestFlight): distribute the app for testing (#34)
    Close: https://github.com/flyve-mdm/flyve-mdm-ios-inventory-agent/issues/28 and https://github.com/flyve-mdm/flyve-mdm-ios-inventory-agent/issues/33

--- scripts/run-build.sh
+++ scripts/run-build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-if [[ "$TRAVIS_BRANCH" == "develop" ]]; then
+if [[ "$TRAVIS_BRANCH" == "develop" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
   fastlane beta
+elif [[ "$TRAVIS_BRANCH" == "develop" && "$TRAVIS_PULL_REQUEST" == "true" ]]; then
+  fastlane test
 elif [[ "$TRAVIS_BRANCH" == "master" ]]; then
-  fastlane release
+  fastlane test
 else
   fastlane test
 fi
