b754913d47b428189181009795dfdef9ea789715
Hector Rondon <hrondon@teclib.com>
2017-09-09 23:01:52 -0400

    ci(fastlane): add Fastlane environment variables

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -2,23 +2,23 @@
 
 #   Copyright © 2017 Teclib. All rights reserved.
 #
-# before_install.sh is part of flyve-mdm-ios
+# before_install.sh is part of FlyveMDMInventoryAgent
 #
-# flyve-mdm-ios is a subproject of Flyve MDM. Flyve MDM is a mobile
+# FlyveMDMInventoryAgent is a subproject of Flyve MDM. Flyve MDM is a mobile
 # device management software.
 #
-# flyve-mdm-ios is free software: you can redistribute it and/or
+# FlyveMDMInventoryAgent is free software: you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 3
 # of the License, or (at your option) any later version.
 #
-# flyve-mdm-ios is distributed in the hope that it will be useful,
+# FlyveMDMInventoryAgent is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 # ------------------------------------------------------------------------------
 # @author    Hector Rondon
-# @date      03/09/17
+# @date      25/08/17
 # @copyright Copyright © 2017 Teclib. All rights reserved.
 # @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
 # @link      https://github.com/flyve-mdm/flyve-mdm-ios-inventory-agent
@@ -29,5 +29,5 @@ echo ----------- Create Fastlane environment variables ------------
 # Create Fastlane environment variables
 echo FASTLANE_PASSWORD=$FASTLANE_PASSWORD >> .env
 echo TELEGRAM_WEBHOOKS=$TELEGRAM_WEBHOOKS >> .env
-echo GIT_REPO=$TRAVIS_REPO_SLUG >> .env
-echo GIT_BRANCH=$TRAVIS_BRANCH >> .env
+echo GIT_REPO=$GH_REPO_SLUG >> .env
+echo GIT_BRANCH=$CIRCLE_BRANCH >> .env
