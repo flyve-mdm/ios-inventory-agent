fb0ab78d7c5d3b04bed1907a492de4b7c2843b8a
Hector Rondon <hrondon@teclib.com>
2017-09-09 23:06:59 -0400

    ci(circle): rename script files

--- ci/before_test.sh
+++ ci/before_test.sh
@@ -2,28 +2,55 @@
 
 #   Copyright © 2017 Teclib. All rights reserved.
 #
-# before_script.sh is part of flyve-mdm-ios
+# before_test.sh is part of FlyveMDMInventoryAgent
 #
-# flyve-mdm-ios is a subproject of Flyve MDM. Flyve MDM is a mobile
+# FlyveMDMInventoryAgent is a subproject of Flyve MDM. Flyve MDM is a mobile
 # device management software.
 #
-# flyve-mdm-ios is free software: you can redistribute it and/or
+# FlyveMDMInventoryAgent is free software: you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 3
 # of the License, or (at your option) any later version.
 #
-# flyve-mdm-ios is distributed in the hope that it will be useful,
+# FlyveMDMInventoryAgent is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 # ------------------------------------------------------------------------------
 # @author    Hector Rondon
-# @date      03/09/17
+# @date      25/08/17
 # @copyright Copyright © 2017 Teclib. All rights reserved.
 # @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
 # @link      https://github.com/flyve-mdm/flyve-mdm-ios-inventory-agent
 # @link      https://flyve-mdm.com
 # ------------------------------------------------------------------------------
+
+echo ------------------- Configure Transifex --------------------
+# Configure Transifex
+if [[ ("$CIRCLE_BRANCH" == "develop" || "$CIRCLE_BRANCH" == "master") && "$CI_PULL_REQUEST" == "" ]]; then
+    # Create config file transifex
+    sudo echo $'[https://www.transifex.com]\nhostname = https://www.transifex.com\nusername = '"$TRANSIFEX_USER"$'\npassword = '"$TRANSIFEX_API_TOKEN"$'\ntoken = '"$TRANSIFEX_API_TOKEN"$'\n' > ~/.transifexrc
+    
+    # Move to local branch
+    git checkout $CIRCLE_BRANCH -f
+    # get transifex status
+    tx status
+    # push local files to transifex
+    tx push -s -t
+    # pull all the new language
+    tx pull -a
+
+    if [[ -n $GH_TOKEN ]]; then
+        git config --global user.email $GH_EMAIL
+        git config --global user.name "Flyve MDM"
+        git remote remove origin
+        git remote add origin https://$GH_USER:$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git
+    fi
+
+    git add .
+    git commit -m "ci(localization): download languages from **Transifex**"
+    git push origin $CIRCLE_BRANCH
+fi
 
 echo ----------------- Decrypt custom keychain ------------------
 # Decrypt custom keychain
@@ -36,7 +63,7 @@ security create-keychain -p $KEYCHAIN_PASSWORD $KEYCHAIN_NAME
 security add-certificates -k $KEYCHAIN_NAME $CERTIFICATES_PATH/apple.cer $CERTIFICATES_PATH/dist.cer
 echo ------------ Make the custom keychain default, so xcodebuild will use it for signing -------------
 # Make the custom keychain default, so xcodebuild will use it for signing
-# security list-keychains -d user -s $KEYCHAIN_NAME
+security list-keychains -d user -s $KEYCHAIN_NAME
 security default-keychain -s $KEYCHAIN_NAME
 echo ------------------- Unlock the keychain --------------------
 # Unlock the keychain
