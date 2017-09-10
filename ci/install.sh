f753e28ca11d55fb19410a7191b2cdc77fe05590
Hector Rondon <hrondon@teclib.com>
2017-09-09 23:03:33 -0400

    ci(localization): install transifex-client

--- ci/install.sh
+++ ci/install.sh
@@ -2,23 +2,23 @@
 
 #   Copyright © 2017 Teclib. All rights reserved.
 #
-# install.sh is part of flyve-mdm-ios
+# install.sh is part of FlyveMDMInventoryAgent
 #
-# flyve-mdm-ios is a subproject of Flyve MDM. Flyve MDM is a mobile
+# FlyveMDMInventoryAgent is a subproject of Flyve MDM. Flyve MDM is a mobile
 # device management software.
 #
-# flyve-mdm-ios is free software: you can redistribute it and/or
+# FlyveMDMInventoryAgent is free software: you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 3
 # of the License, or (at your option) any later version.
 #
-# flyve-mdm-ios is distributed in the hope that it will be useful,
+# FlyveMDMInventoryAgent is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 # ------------------------------------------------------------------------------
 # @author    Hector Rondon
-# @date      03/09/17
+# @date      25/08/17
 # @copyright Copyright © 2017 Teclib. All rights reserved.
 # @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
 # @link      https://github.com/flyve-mdm/flyve-mdm-ios-inventory-agent
@@ -35,8 +35,13 @@ gem cleanup
 gem install jazzy
 # Install xcov for code coverage reporting
 sudo gem install xcov
+# Install node
+brew install node
 # Install jq for json parse
 brew install jq
+# Install transifex-client
+sudo easy_install pip
+sudo pip install transifex-client
 # Install standard-version scope global
 npm i -g standard-version
 # Install conventional-github-releaser scope global
