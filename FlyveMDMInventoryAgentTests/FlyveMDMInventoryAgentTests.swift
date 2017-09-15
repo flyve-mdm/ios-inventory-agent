c8eff3cbe04de13a8bcbc3ad205050572561001c
Naylin <nmedina@teclib.com>
2017-09-15 15:18:57 -0400

    docs(tearDown): change the description

--- FlyveMDMInventoryAgentTests/FlyveMDMInventoryAgentTests.swift
+++ FlyveMDMInventoryAgentTests/FlyveMDMInventoryAgentTests.swift
@@ -44,8 +44,8 @@ class FlyveMDMInventoryAgentTests: XCTestCase {
         window?.rootViewController = navigationController
     }
 
+    /// This method is called after the invocation of each test method in the class.
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
 
