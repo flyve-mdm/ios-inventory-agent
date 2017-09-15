51ae755f6533b36f0d1dce14c571eead1e4ebf15
Naylin <nmedina@teclib.com>
2017-09-15 17:00:52 -0400

    docs(setUp): change the description

--- FlyveMDMInventoryAgentUITests/FlyveMDMInventoryAgentUITests.swift
+++ FlyveMDMInventoryAgentUITests/FlyveMDMInventoryAgentUITests.swift
@@ -29,10 +29,10 @@ import XCTest
 
 class FlyveMDMInventoryAgentUITests: XCTestCase {
 
+    /// This method is called before the invocation of each test method in the class.
     override func setUp() {
         super.setUp()
 
-        // Put setup code here. This method is called before the invocation of each test method in the class.
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
