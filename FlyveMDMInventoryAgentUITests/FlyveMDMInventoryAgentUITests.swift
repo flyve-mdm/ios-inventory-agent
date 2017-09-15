65078846488c445a9927113114238f2353946d96
Naylin <nmedina@teclib.com>
2017-09-15 16:59:11 -0400

    docs(tearDown): change the description

--- FlyveMDMInventoryAgentUITests/FlyveMDMInventoryAgentUITests.swift
+++ FlyveMDMInventoryAgentUITests/FlyveMDMInventoryAgentUITests.swift
@@ -43,9 +43,9 @@ class FlyveMDMInventoryAgentUITests: XCTestCase {
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-
+    
+    /// This method is called after the invocation of each test method in the class.
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
 
