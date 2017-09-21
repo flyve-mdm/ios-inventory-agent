4550f47b44b6c57dbee5c14749e68e08fbad976c
Hector Rondon <hrondon@teclib.com>
2017-09-21 15:43:13 -0400

    ci(sign): remove provisioning profile

--- ci/scripts/keychain_add.sh
+++ ci/scripts/keychain_add.sh
@@ -27,7 +27,6 @@
 
 echo ----------------- Decrypt custom keychain ------------------
 # Decrypt custom keychain
-openssl aes-256-cbc -k "$KEYCHAIN_PASSWORD" -in $PROFILE_PATH/$PROFILE_UUID.mobileprovision.enc -d -a -out $PROFILE_PATH/$PROFILE_UUID.mobileprovision
 openssl aes-256-cbc -k "$KEYCHAIN_PASSWORD" -in $CERTIFICATES_PATH/dist.cer.enc -d -a -out $CERTIFICATES_PATH/dist.cer
 openssl aes-256-cbc -k "$KEYCHAIN_PASSWORD" -in $CERTIFICATES_PATH/dist.p12.enc -d -a -out $CERTIFICATES_PATH/dist.p12
 echo -------------Create the keychain with a password -----------
@@ -52,7 +51,3 @@ security import $CERTIFICATES_PATH/dist.cer -k ~/Library/Keychains/$KEYCHAIN_NAM
 security import $CERTIFICATES_PATH/dist.p12 -k ~/Library/Keychains/$KEYCHAIN_NAME -P "$KEYCHAIN_PASSWORD" -T /usr/bin/codesign
 
 security set-key-partition-list -S apple-tool:,apple: -s -k $KEYCHAIN_PASSWORD $KEYCHAIN_NAME
-echo -------------- Put the provisioning profile ----------------
-# Put the provisioning profile in place
-mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-cp "$PROFILE_PATH/$PROFILE_UUID.mobileprovision" ~/Library/MobileDevice/Provisioning\ Profiles/
