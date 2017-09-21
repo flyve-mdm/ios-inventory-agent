c78697ece157d4977e472d435c3e492d879fc408
Hector Rondon <hrondon@teclib.com>
2017-09-21 16:31:15 -0400

    ci(fastlane): add environment variables to circle CI

--- ci/scripts/fastlane_config.sh
+++ ci/scripts/fastlane_config.sh
@@ -29,7 +29,7 @@ echo ----------- Create Fastlane environment variables ------------
 # Create Fastlane environment variables
 echo FASTLANE_PASSWORD=$FASTLANE_PASSWORD >> .env
 echo TELEGRAM_WEBHOOKS=$TELEGRAM_WEBHOOKS >> .env
-echo GIT_REPO=$GITHUB_REPO_SLUG >> .env
+echo GIT_REPO="$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME" >> .env
 echo GIT_BRANCH=$CIRCLE_BRANCH >> .env
 echo APPLE_ID=$APPLE_ID >> .env
 echo APPLE_TEAM_ID=$APPLE_TEAM_ID >> .env
--- ci/scripts/git_config.sh
+++ ci/scripts/git_config.sh
@@ -29,5 +29,5 @@ if [[ -n $GITHUB_TOKEN ]]; then
     git config --global user.email $GITHUB_EMAIL
     git config --global user.name "Flyve MDM"
     git remote remove origin
-    git remote add origin https://$GITHUB_USER:$GITHUB_TOKEN@github.com/$GITHUB_REPO_SLUG.git
+    git remote add origin https://$GITHUB_USER:$GITHUB_TOKEN@github.com/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME.git
 fi
