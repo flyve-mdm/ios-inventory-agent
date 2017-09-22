fb5f25a84fd6fcabab99e75cb99185f2253155b2
Hector Rondon <hrondon@teclib.com>
2017-09-22 11:33:02 -0400

    test(screenshot): generate screenshot agent settings

--- FlyveMDMInventoryAgentUITests/FlyveMDMInventoryAgentUITests.swift
+++ FlyveMDMInventoryAgentUITests/FlyveMDMInventoryAgentUITests.swift
@@ -28,6 +28,8 @@
 import XCTest
 
 class FlyveMDMInventoryAgentUITests: XCTestCase {
+    
+    let app = XCUIApplication()
 
     /// This method is called before the invocation of each test method in the class.
     override func setUp() {
@@ -37,9 +39,9 @@ class FlyveMDMInventoryAgentUITests: XCTestCase {
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
 
-        XCUIApplication().launchEnvironment = [ "UITest": "1" ]
-        setupSnapshot(XCUIApplication())
-        XCUIApplication().launch()
+        app.launchEnvironment = [ "UITest": "1" ]
+        setupSnapshot(app)
+        app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -48,10 +50,15 @@ class FlyveMDMInventoryAgentUITests: XCTestCase {
     override func tearDown() {
         super.tearDown()
     }
-
-    func testExample() {
-        snapshot("0Launch")
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    /// This method take screenshots from fastlane snapshot
+    func testTakeScreenshots() {
+        
+        app.tap()
+        snapshot("01Screen")
+        let cells = app.tables.cells
+        let lastCell = cells.element(boundBy: cells.count - 1)
+        lastCell.tap()
+        snapshot("02Screen")
     }
 }
