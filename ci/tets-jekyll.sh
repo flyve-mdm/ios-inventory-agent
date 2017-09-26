bca533c5ec088ceb909b13ec19d1a6e3d0bb0d9f
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-09-26 13:37:24 -0400

    docs(jekyll): improve to pass the tests

--- ci/tets-jekyll.sh
+++ ci/tets-jekyll.sh
@@ -2,5 +2,5 @@
 set -e # halt script on error
 
 bundle exec jekyll build
-rm -rf _site/reports
+rm -rf _site/docs
 bundle exec htmlproofer ./_site --allow-hash-href true --assume-extension true --file-ignore ./_site/CHANGELOG.html
