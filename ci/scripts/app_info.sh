dd2e4155fad217afab4777f661aaf8c52cb9e0ff
Hector Rondon <hrondon@teclib.com>
2017-09-28 11:35:09 -0400

    ci(about): create file about app information

---
+++ ci/scripts/app_info.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+#   Copyright © 2017 Teclib. All rights reserved.
+#
+# bundler_config.sh is part of FlyveMDMInventoryAgent
+#
+# FlyveMDMInventoryAgent is a subproject of Flyve MDM. Flyve MDM is a mobile
+# device management software.
+#
+# FlyveMDMInventoryAgent is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# FlyveMDMInventoryAgent is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# ------------------------------------------------------------------------------
+# @author    Hector Rondon
+# @date      28/09/17
+# @copyright Copyright © 2017 Teclib. All rights reserved.
+# @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+# @link      https://github.com/flyve-mdm/flyve-mdm-ios-inventory-agent
+# @link      https://flyve-mdm.com
+# ------------------------------------------------------------------------------
+
+GIT_TAG=$(jq -r ".version" package.json)
+
+echo "\"version\" = \"$GIT_TAG\";" > "$APPNAME/about.strings"
+echo "\"build\" = \"188\";" >> "$APPNAME/about.strings"
+echo "\"date\" = \"$(date "+%a %b %d %H:%M:%S %Y")\";" >> "$APPNAME/about.strings"
+echo "\"commit\" = \"$(git rev-parse --verify --short=7 HEAD)\";" >> "$APPNAME/about.strings"
+echo "\"commit_full\" = \"$(git rev-parse --verify HEAD)\";" >> "$APPNAME/about.strings"
