deb23fef08e4dfcd595c01edc6442d907cbf6b42
Hector Rondon <hrondon@teclib.com>
2017-09-28 20:13:12 -0400

    feat(notifications): create extension for hiden notifications

---
+++ FlyveMDMInventoryAgent/Delay+Helper.swift
@@ -0,0 +1,38 @@
+/*
+ *   Copyright © 2017 Teclib. All rights reserved.
+ *
+ * Delay+Helper.swift is part of FlyveMDMInventoryAgent
+ *
+ * FlyveMDMInventoryAgent is a subproject of Flyve MDM. Flyve MDM is a mobile
+ * device management software.
+ *
+ * FlyveMDMInventoryAgent is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * FlyveMDMInventoryAgent is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * ------------------------------------------------------------------------------
+ * @author    Hector Rondon
+ * @date      28/06/17
+ * @copyright Copyright © 2017 Teclib. All rights reserved.
+ * @license   LGPLv3 https://www.gnu.org/licenses/lgpl-3.0.html
+ * @link      https://github.com/flyve-mdm/flyve-mdm-ios-inventory-agent.git
+ * @link      https://flyve-mdm.com
+ * ------------------------------------------------------------------------------
+ */
+
+import Foundation
+
+/**
+ Delay time
+ 
+ - parameter delay: delay time in seconds, default value = 3.0
+ - parameter closure: @escaping () -> Void
+ */
+public func delay(_ delay: Double = 3.0, closure: @escaping () -> Void) {
+    DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + Double(Int64(delay * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC), execute: closure)
+}
