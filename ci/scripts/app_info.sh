de228d3ad851c11df20963de06cea3c72550c618
Hector Rondon <hrondon@teclib.com>
2017-10-11 09:36:33 -0400

    ci(build): update build info

--- ci/scripts/app_info.sh
+++ ci/scripts/app_info.sh
@@ -2,7 +2,7 @@
 
 #   Copyright © 2017 Teclib. All rights reserved.
 #
-# bundler_config.sh is part of FlyveMDMInventoryAgent
+# app_info.sh is part of FlyveMDMInventoryAgent
 #
 # FlyveMDMInventoryAgent is a subproject of Flyve MDM. Flyve MDM is a mobile
 # device management software.
@@ -25,10 +25,15 @@
 # @link      https://flyve-mdm.com
 # ------------------------------------------------------------------------------
 
+# Get version app
 GIT_TAG=$(jq -r ".version" package.json)
-
+# Update about.strings file
 echo "\"version\" = \"$GIT_TAG\";" > "$APPNAME/about.strings"
 echo "\"build\" = \"$CIRCLE_BUILD_NUM\";" >> "$APPNAME/about.strings"
 echo "\"date\" = \"$(date "+%a %b %d %H:%M:%S %Y")\";" >> "$APPNAME/about.strings"
 echo "\"commit\" = \"${CIRCLE_SHA1:0:7}\";" >> "$APPNAME/about.strings"
 echo "\"commit_full\" = \"$CIRCLE_SHA1\";" >> "$APPNAME/about.strings"
+# Add about.strings to git
+git add -u
+# Create commit
+git commit -m "ci(build): update build info to version ${GIT_TAG}"
