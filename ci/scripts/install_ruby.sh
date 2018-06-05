0eb8c68d7738b6913d5def65510394c20a71e4c2
Hector Rondon <hrondon@teclib.com>
2018-06-05 16:15:56 -0400

    ci(build): configure ruby dependencies
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

---
+++ ci/scripts/install_ruby.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+#   Copyright © 2017 Teclib. All rights reserved.
+#
+# install.sh is part of FlyveMDMInventoryAgent
+#
+# FlyveMDMInventoryAgent is a subproject of Flyve MDM. Flyve MDM is a mobile
+# device management software.
+#
+# FlyveMDMInventoryAgent is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# FlyveMDMInventoryAgent is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# ------------------------------------------------------------------------------
+# @author    Hector Rondon
+# @date      06/06/18
+# @copyright Copyright © 2017 Teclib. All rights reserved.
+# @license   LGPLv3 https://www.gnu.org/licenses/lgpl-3.0.html
+# @link      https://github.com/flyve-mdm/flyve-mdm-ios-inventory-agent
+# @link      https://flyve-mdm.com
+# ------------------------------------------------------------------------------
+
+# update ruby
+brew install ruby
+# Update gem
+gem update --system
+# Clean Gem
+gem cleanup
+# Install gems from Gemfile
+bundle install --path vendor/bundle
+# Update fastlane plugin
+bundle exec fastlane update_plugins
